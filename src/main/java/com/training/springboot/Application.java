fec6224e97f033d1dc32bc7744c7eb4ccd53a916
Andrei Kaliachka <akaliachka@gmail.com>
2019-04-02 18:03:48 +0300

    feature(db): set up mysql connection and liquibase
     Please enter the commit message for your changes. Lines starting

--- src/main/java/com/training/springboot/Application.java
+++ src/main/java/com/training/springboot/Application.java
@@ -1,12 +1,15 @@
 package com.training.springboot;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class Application {
 
     public static void main(String[] args) {
+        log.debug("Hello {}", args[0]);
         SpringApplication.run(Application.class, args);
     }
 
